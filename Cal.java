d885455bca3e583810213c9cdb54ebc5547b41f5
Akshay Patil <akshay.patil@cuelogic.com>
2019-09-25 19:03:11 +0530

    Multiplication method added

--- Cal.java
+++ Cal.java
@@ -4,8 +4,12 @@ public class Cal{
 		System.out.println(a+b);
 	}
 
+	public static void mul(int a, int b){
+		System.out.println(a*b);
+	}
 
 	public static void main(String args[]){
 		add(10,20);
+		mul(15,3);
 	}
 }
